d90b99e15e730b8905469e701f0b655c3057d646
Bhargav <33828401+bhargav-bkpatel@users.noreply.github.com>
2022-04-22 21:45:47 +0530

    Remove Animated cursor.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,18 @@
 import React, { Fragment } from "react"
 import Nav from '../components/Nav'
 import '../styles/globalstyles.scss'
-import AnimatedCursor from "react-animated-cursor"
+//import AnimatedCursor from "react-animated-cursor"
 // markup
 const IndexPage = () => {
   return (
     <Fragment>
-       <AnimatedCursor
+       {/* <AnimatedCursor
       innerSize={0}
       outerSize={10}
       color='255,0,0'
       outerAlpha={10}
       outerScale={1}      
-    />
+    /> */}
       <Nav />
       <main>
       </main >
